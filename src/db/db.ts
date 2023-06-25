3d4b7600e92f8d50104eafcbd25d388aad30f437
DESKTOP-49DLLO2\benja <benjamin.strumeyer@gmail.com>
2023-06-24 23:16:18 -0400

    feat: insert a song into the database

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,8 +1,9 @@
 import { MongoClient } from 'mongodb';
+import { Song } from '../../types/index';
 
 const mongoURL = 'mongodb://localhost:27017'; // Replace with your MongoDB connection string
-const dbName = 'lyrics'; // Replace with your database name
-
+const dbName = 'rhyme-time'; // Replace with your database name
+const uri = 'mongodb://localhost:27017'; // Replace with your MongoDB connection string
 let db: any;
 
 // Connect to the MongoDB database
@@ -17,12 +18,19 @@ export async function connectDb() {
   }
 }
 
-// Insert lyrics into the database
-export async function insertLyrics(artist: string, lyrics: string) {
-  const collection = db.collection('lyrics');
-  const document = {
-    artist: artist,
-    lyrics: lyrics
-  };
-  await collection.insertOne(document);
+export async function insertSong(song: Song): Promise<void> {
+  const client = new MongoClient(uri);
+
+  try {
+    await client.connect();
+    const db = client.db(dbName);
+    const collection = db.collection('song');
+
+    await collection.insertOne(song);
+    console.log('Song inserted successfully');
+  } catch (error) {
+    console.error('Error inserting song:', error);
+  } finally {
+    client.close();
+  }
 }
--- src/server.ts
+++ src/server.ts
@@ -1,35 +1,37 @@
 // const express = require('express');
 
-import { connectDb, insertLyrics } from './db/db'; // Assuming you've created the `db.js` file
+import { connectDb, insertSong } from './db/db'; // Assuming you've created the `db.js` file
 
 import express, { Request, Response } from 'express';
-import { MongoClient } from 'mongodb';
 import { find_lyrics } from './utils/find_lyrics';
 
 
 const app = express();
 const port = 3000;
 
-const mongoURL = 'mongodb://localhost:27017'; // Replace with your MongoDB connection string
-const dbName = 'lyrics'; // Replace with your database name
-
 // Middleware to parse JSON requests
 app.use(express.json());
 
 // Route to retrieve lyrics for a specific artist
-// app.get('/lyrics/:artist', async (req: Request, res: Response) => {
 app.get('/lyrics', async (req: Request, res: Response) => {
   try {
     const response = await find_lyrics("Wap");
-
-    const lyrics = response;
     console.log('Response: ', response);
-
-    // res.status(200).json({ success: true, message: 'Lyrics stored successfully.' });
+    res.status(200).json({ success: true, message: 'Lyrics stored successfully.' });
   } catch (error) {
     console.error(error);
-    // res.status(500).json({ success: false, message: 'Failed to retrieve and store lyrics.' });
+    res.status(500).json({ success: false, message: 'Failed to retrieve and store lyrics.' });
   }
+});
+
+app.post('/song', async () => {
+  const song = {
+    name: 'WAP',
+    artist: 'Cardi B',
+    lyrics: 'some lyrics'
+  };
+
+  const response = await insertSong(song);
 });
 
 // Connect to the MongoDB database and start the server
